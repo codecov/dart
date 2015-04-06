af21f8ea9e052e714d6ca186e7e6066c3c5dc70f
John Ryan <john.ryan@workiva.com>
2015-04-06 15:59:31 -0500

    add gen-html flag

--- bin/generate_coverage.dart
+++ bin/generate_coverage.dart
@@ -1,13 +1,21 @@
 import "dart:io";
 import "dart:async";
 import "dart:convert";
+import "package:args/args.dart";
 
-main(List<String> args) async {
-  if (args.length < 1) {
+main(List<String> arguments) async {
+  var parser = new ArgParser();
+  parser.addFlag('gen-html', defaultsTo: true);
+  var results = parser.parse(arguments);
+  var genHtml = results['gen-html'];
+  arguments = results.rest;
+
+  if (arguments.length < 1) {
     print("No test file given. Please specify a test file.");
     exit(1);
   }
-  var file = args[0];
+
+  var file = arguments[0];
   print('collecting coverage for $file');
 
   Process testProcess;
@@ -36,10 +44,17 @@ main(List<String> args) async {
     'lcov_coverage.lcov'
   ]);
 
-  print('generating html');
-  await Process.run('genhtml', ['-o', 'lcov_report', 'lcov_coverage.lcov']);
+  if (genHtml) {
+    print('generating html');
+    await Process.run('genhtml', ['-o', 'lcov_report', 'lcov_coverage.lcov']);
+  }
 
-  print('Coverge generated. Open lcov_report/index.html');
+
+  if (genHtml) {
+    print('Coverge generated. Open lcov_report/index.html');
+  } else {
+    print('Coverage generated.  Results saved to lcov_coverage.lcov');
+  }
   dir.delete(recursive: true);
   testProcess.kill();
   exit(0);
@@ -47,7 +62,7 @@ main(List<String> args) async {
 
 // attempts to run tests with the default observe port, 8444
 Future<Process> attemptToRunTests(String file) {
-  var completer = new Completer<bool>();
+  var completer = new Completer<Process>();
   Process.start('dart', ['--observe=8444', file]).then((Process p) {
     p.stdout.transform(UTF8.decoder).takeWhile((_) => !completer.isCompleted).listen((data) {
       var failed = new RegExp(r"SocketException");
@@ -59,6 +74,8 @@ Future<Process> attemptToRunTests(String file) {
         completer.complete(p);
       }
     });
+  }).catchError((e) {
+    completer.completeError("error running tests: $e");
   });
   return completer.future;
 }
