d4cd3943a5e718b90610bda931a52685cb936ebc
ekweible <ekweible@gmail.com>
2015-05-06 14:38:10 -0500

    Simplify executable name.

---
+++ bin/dart_codecov_generator.dart
@@ -0,0 +1,3 @@
+library dart_codecov_generator.bin.dart_codecov_generator;
+
+export 'src/executable.dart';
--- bin/generate_coverage.dart
+++ bin/generate_coverage.dart
@@ -1,3 +1,10 @@
 library dart_codecov_generator.bin.generate_coverage;
 
-export 'src/executable.dart';
+import 'src/executable.dart' as executable;
+
+
+void main(List<String> args) {
+  print('Warning: `pub run dart_codecov_generator:generate_coverage` is deprecated.');
+  print('Use `pub run dart_codecov_generator` instead.\n');
+  executable.main(args);
+}
