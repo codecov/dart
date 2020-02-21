1e04fca84d358223309d9295f50b9fed792405d9
John Ryan <ryjohn@google.com>
2020-02-21 12:41:15 -0800

    Update project to Dart 2 and unittest
    Signed-off-by: Joe Becher <jwbecher@drazisil.com>

--- test/browser_test.dart
+++ test/browser_test.dart
@@ -4,9 +4,6 @@ library dart_codecov_generator.test.browser_test;
 import 'dart:html';
 
 import 'package:test/test.dart';
-import 'package:unittest/html_config.dart';
-
-useHtmlConfiguration(true);
 
 bool _dummyCoverage() => true;
 
