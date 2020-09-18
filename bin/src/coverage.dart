a47a7e49d508513277c73fc3a2e9b721c1d2fc53
Thomas Hu <tomhu1096@gmail.com>
2020-09-18 15:24:35 -0400

    Oops base

--- bin/src/coverage.dart
+++ bin/src/coverage.dart
@@ -51,8 +51,8 @@ class Coverage {
       log.shout('Entities ${entities}');
       for (FileSystemEntity entity in entities) {
         if (entity is File) {
-          log.shout('Renaming: ${entity.path} to ${coverageDir.path}/$base');
           String base = path.basename(entity.path);
+          log.shout('Renaming: ${entity.path} to ${coverageDir.path}/$base');
           entity.rename('${coverageDir.path}/$base');
 
           bool fileExists = await File('${coverageDir.path}/$base').exists();
