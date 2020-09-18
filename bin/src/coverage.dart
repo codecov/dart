4ba9d167fef51dee5770426df756b2611d219586
Thomas Hu <tomhu1096@gmail.com>
2020-09-18 00:13:00 -0400

    Logs

--- bin/src/coverage.dart
+++ bin/src/coverage.dart
@@ -37,6 +37,7 @@ class Coverage {
     if (coverages.length == 0) throw new ArgumentError('Cannot merge an empty list of coverages.');
     Coverage merged = new Coverage(null);
 
+    Logger log = new Logger('dcg');
     log.shout(await coverages[0].collectionOutput.readAsString());
     Map mergedJson = json.decode(await coverages[0].collectionOutput.readAsString());
     for (int i = 1; i < coverages.length; i++) {
